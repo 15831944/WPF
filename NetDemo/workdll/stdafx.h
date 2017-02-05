89f79c220d7d9c052f0a49dbd0bbf9e5b1335bbf
rox00 <726372432@qq.com>
2017-02-05 14:08:16 +0800

    修改代码

--- NetDemo/workdll/stdafx.h
+++ NetDemo/workdll/stdafx.h
@@ -16,7 +16,7 @@
 // TODO:  在此处引用程序需要的其他头文件
 
 #ifdef _DEBUG
-#pragma comment( lib, "libprotobuf.lib" )
+#pragma comment( lib, "libprotobuf_D.lib" )
 #else
 #pragma comment( lib, "libprotobuf.lib" )
 #endif
